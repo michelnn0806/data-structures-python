4fbec6ed578736dc7014800370b3fcc7f4c49740
Michel <c5034687@newcastle.ac.uk>
2026-09-05 03:54:24 +0100

    Made a loop to manage and execute operations on the queue based on what options where picked

--- Queue.py
+++ Queue.py
@@ -1,9 +1,11 @@
 #A queue is a first in first out data structure
+
 Queue = []
 
 #When an item is enqueued it goes the back of the queue
-def enqueue(item):
-    Queue.append(item)
+def enqueue():
+    number = int(input("Enter a number: "))
+    Queue.append(number)
 
 #When an item is dequeued the item at the front of the queue is removed
 def dequeue():
@@ -15,12 +17,43 @@ def peek():
 def size():
     print(len(Queue))
 
-def IsEmpty():
+def QueueEmpty():
     if len(Queue) == 0:
-        print("Queue is empty")
+        return True
+    else:
+        return False
 
 menu = ("1. Enqueue \n"
            "2. dequeue \n" 
            "3. peek \n" 
            "4. size \n" 
            "5. exit")
+
+again = True
+
+while again:
+    print(menu)
+    print(Queue)
+
+    option = int(input("Enter your choice: "))
+
+    if option == 1:
+        enqueue()
+
+    elif option == 2:
+        if QueueEmpty():
+            print("Queue is empty")
+        else:
+            dequeue()
+
+    elif option == 3:
+        if QueueEmpty():
+            print("Queue is empty")
+        else:
+            peek()
+
+    elif option == 4:
+        size()
+
+    elif option == 5:
+        again = False
